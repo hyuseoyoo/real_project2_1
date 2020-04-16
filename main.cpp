1f9b55aca7941fd035cdcf5ea32a1bd99349a4d8
Hyuseo Yoo <hyu9612@naver.com>
2020-04-16 02:38:22 -0700

    hw2-2 done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include "sorting.h"
 
 int sorted[10];
+int num[10];
 
 int main(){
+    for(int i=0; i<10; i++) {
+        scanf("%d", &num[i]);
+    }
+    
+    merge_sort(num, 0, 9);
+
+    for(int i=0; i<10; i++)
+       printf("%d ", sorted[i]);
+
+    printf("\n");    
 
     return 0;
 }
