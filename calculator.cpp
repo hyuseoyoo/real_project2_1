ce7daa5762f24e96dade7f322d65eab1511c84dd
Hyuseo Yoo <hyu9612@naver.com>
2020-04-28 04:43:29 -0700

    new hw done

--- calculator.cpp
+++ calculator.cpp
@@ -9,9 +9,11 @@ void subtraction(){
 }
 
 void multiplication(){
-
+    int answer = a * b;
+    printf("%d * %d = %d\n", a, b, answer);
 }
 
 void division(){
-    
+    float answer = (float)(a) / (float)(b);
+    printf("%d / %d = %f\n", a, b, answer);
 }
